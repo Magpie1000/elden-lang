f368fc52ac12a641d8dcec26ffa57e2b4a0e0677
Magpie1000 <web.1000c@gmail.com>
2022-05-24 00:25:50 +0900

    FEAT: ✨Landing page card add ed

--- src/views/main/Landing.vue
+++ src/views/main/Landing.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="mb-0" style="min-height: 90vh">
     <v-card
       style="background-color: rgba(0, 0, 0, 0.8)"
-      min-height="500"
       class="text-center py-10 align-center"
+      min-height="450"
     >
       <v-card-title
         class="justify-center pt-15 mt-5 mb-10"
@@ -19,33 +19,73 @@
         >Prepare to debug</v-card-text
       >
       <v-card-actions class="justify-center pt-8">
-        <v-btn>Epic Boss Fight</v-btn>
-        <v-btn>The Golden Order</v-btn>
+        <v-btn x-large>Epic Boss Fight</v-btn>
+        <v-btn x-large>The Golden Order</v-btn>
       </v-card-actions>
     </v-card>
-    <v-container>
+    <div class="pt-10" style="background-color: rgba(0, 0, 0, 0.5)">
       <v-card
-        class="justify-center"
+        class="justify-center py-5"
         min-height="300"
         style="background-color: rgba(0, 0, 0, 0); color: white"
       >
-        <v-row class="align-self-center">
+        <v-row class="justify-center align-self-center">
           <v-card-title style="font-size: 40px">
-            Need an epic bossfight?
+            Need an<br />
+            epic bossfight?
           </v-card-title>
-          <!-- <v-img
+          <v-img
             max-width="300"
             src="https://dcimg4.dcinside.co.kr/viewimage.php?id=2bafdf2bf6dd3eb279bec4b0&no=24b0d769e1d32ca73feb80fa11d028315f52e6e1fc8bbd14245a9b9fd2ad519786299874f0e5852dcefd12920eb4d3a3e503cc024c392ab9d40807186b803f15d95b2e70dd67e7"
-          /> -->
+          />
         </v-row>
       </v-card>
-      <v-card>
-        <v-card-title> while you are fighting with bugs? </v-card-title>
+      <v-card
+        class="justify-center py-5"
+        min-height="300"
+        style="background-color: rgba(0, 0, 0, 0); color: white"
+      >
+        <v-row class="justify-center align-self-center pt-10">
+          <v-img
+            max-width="300"
+            src="https://img-9gag-fun.9cache.com/photo/arGv6B0_460s.jpg"
+          />
+          <v-card-title style="font-size: 40px">
+            Have to fight <br />against bugs?
+          </v-card-title>
+        </v-row>
       </v-card>
-      <v-card>
-        <v-card-title> Life is too short. You need 'Elden-lang'. </v-card-title>
+      <v-card
+        class="justify-center text-center py-5"
+        min-height="300"
+        style="background-color: rgba(0, 0, 0, 0); color: white"
+      >
+        <v-row class="justify-center align-self-center">
+          <v-card-title class="text-center" style="font-size: 40px">
+            Life is TOO short. <br />You need 'Elden-Lang'.
+          </v-card-title>
+          <v-img
+            max-width="300"
+            src="https://preview.redd.it/0gp7pfwvwrk81.png?width=640&crop=smart&auto=webp&s=e702ede14fd71311dce1f328fb031b4c82679b2d"
+          />
+        </v-row>
       </v-card>
-    </v-container>
+      <v-card
+        class="justify-center align-center text-center py-5"
+        min-height="300"
+        style="background-color: rgba(0, 0, 0, 0); color: white"
+      >
+        <v-btn x-large class="align-self-center mt-15"
+          >Fight Against demigods</v-btn
+        >
+      </v-card>
+      <v-card
+        class="justify-center align-center text-center py-5"
+        min-height="300"
+        style="background-color: rgba(0, 0, 0, 0); color: white"
+      >
+      </v-card>
+    </div>
   </div>
 </template>
 
