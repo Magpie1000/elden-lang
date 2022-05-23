5327f54a4729bff76726d111d8baab921f276f6a
Magpie1000 <web.1000c@gmail.com>
2022-05-23 18:38:12 +0900

    FEAT: ✨  랜딩 페이지 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <landing />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Landing from './main/Landing.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Landing,
   }
 }
 </script>
---
+++ src/views/main/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Landing Page</h1>
+    <v-card>
+      <v-card-title>
+        Need an epic bossfight?
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-card-title>
+        while you are fighting with bugs?
+      </v-card-title>
+    </v-card>
+        <v-card>
+      <v-card-title>
+        Life is too short. You need 'Elden-lang'.
+      </v-card-title>
+    </v-card>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Landing'
+}
+</script>
+
+<style>
+
+</style>
