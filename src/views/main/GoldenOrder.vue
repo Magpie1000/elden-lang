7cf478053f6ed5a0f3921618b4f79f37e72078f4
Magpie1000 <web.1000c@gmail.com>
2022-05-26 08:57:54 +0900

    CHORES: 🍕 Codeblock's font changed and Example code added

--- src/views/main/GoldenOrder.vue
+++ src/views/main/GoldenOrder.vue
@@ -101,6 +101,41 @@
       <h3 class="subheading-text mt-15">
         1. The classic 'Hello, world' sample
       </h3>
+      <v-row class="mt-5" justify="center">
+        <GoldenOrderCodeBlock
+          :text="`Magma Wyrm {\n\n summon jellyfish\n\nr2 r2 r2 r2
+          r2 r2 r2 r2 \n r2 r2 spam roll\nr2 r2 r2 r2 r2 r2 r2 roll\n r2 r2 r2 r2 r2 r2 
+          r2 r2 r2 r2 roll\nr2 r2 r2 roll r2 r1 r1 r1 r1 jump \n\npanic roll \n\nuse flask of tears\n\nr2 r2 guard roll r2 guard 
+          r2 r2 r2 r2 r2 r2 r2 \nguard guard r2 r2 r2 guard roll r2 \nr2 guard r1 r1 r2 r2 r2 r2 \nr2 r2 r2 r2 r2 r2 r2 r2 r2 r2 
+          r2 guard roll guard r2 r2 r2 guard\njump jump jump jump jump jump\n guard jump jump jump \njump jump jump jump jump guard roll\n r2 guard roll guard
+          \n} Enemy felled.`"
+        />
+      </v-row>
+      <h3 class="subheading-text mt-15">2. You died.</h3>
+      <v-row class="mt-5" justify="center">
+        <GoldenOrderCodeBlock
+          :text="`Bloodhound Knight Darriwil { \n\n r2 spam jump jump jump roll 
+          r2 r2 r1 \n\npanic roll\n\n r2 r2 r2\nguard spam\n jump roll r2 r2 r2 r2 r2 r2 r2 r1
+          \npanic roll\n\nguard r2 r2 r2 r2 \nr2 r2 guard jump spam jump\n jump jump jump roll r2 r1 
+          \npanic roll\n\nr2 r2 r2 guard \nr2 spam jump roll r2 r2 \nr2 r1 \n\npanic roll\n
+          r2 guard r2 r2 r2 r2 r2 \nguard jump jump jump jump\nguard jump guard spam jump\nroll r2 r2 r2 r1
+          \npanic roll\n\nr2 r2 guard\n\n} YOU DIED.`"
+        />
+      </v-row>
+      <h3 class="subheading-text mt-15">3. Enemy felled.</h3>
+      <v-row class="mt-5" justify="center">
+        <GoldenOrderCodeBlock
+          :text="`Starscourge Radahn {\n\n r2 r2 r2 r2 \nspam r2\nr2 r2 r2 roll jump jump\njump r1 
+          \npanic roll \n\nuse flask of tears\nuse flask of tears\n\nr2 r2 guard jump spam 
+          jump jump jump roll \nr2 r1 \n\npanic roll\n\n r2 r2 guard jump jump jump \njump jump jump jump
+          jump jump guard r2 r2\nr2 r2 r2 r2 r2 r2\nguard jump jump spam \njump jump jump roll 
+          r2 r1 \n\npanic roll\n\nuse flask of tears\nuse flask of tears\n\nguard jump spam jump 
+          jump jump jump roll \nr2 r1 \n\npanic roll\n\nr2 r2 guard r2 \nr2 spam\njump roll r2 r2
+          r2 r1 \n\npanic roll\n\n guard jump guard r2\n r2 r2 r2 r2 r2 r2\nguard guard jump 
+          jump jump jump jump jump \njump guard jump guard\nspam jump roll r2 r2\nr2 r1 
+          \npanic roll\n\n r2 r2 guard \n\n} ENEMY FELLED.`"
+        />
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -116,6 +151,9 @@ export default {
     GoldenOrderCodeBlock,
   },
   data: () => ({}),
+  created: function () {
+    console.log(this.$route.name);
+  },
 };
 </script>
 
